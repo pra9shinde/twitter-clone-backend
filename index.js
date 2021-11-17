3b49ccb0bdb1f348abfb22e5c7e6c67972695950
Pranav Shinde <46496160+pra9shinde@users.noreply.github.com>
2021-11-17 17:51:04 +0530

    Heroku

--- index.js
+++ index.js
@@ -18,15 +18,17 @@ app.use('/uploads', express.static('uploads')); //Server Static files over Http
 
 server.applyMiddleware({ app });
 
+const PORT = process.env.port || 4000;
+
 //
 mongoose
     .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true, poolSize: 100 })
     .then(() => {
         console.log('MongoDB Connected Successfully');
-        return app.listen({ port: 4000 });
+        return app.listen({ port: PORT });
     })
     .then((res) => {
-        console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+        console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`);
         global.BASE_URL = res.url;
         global.BASE_DIR = __dirname;
     })
