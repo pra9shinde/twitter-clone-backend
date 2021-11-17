35f3d983f962e2a1c118b42a9df109c70aa0825b
Pranav Shinde <46496160+pra9shinde@users.noreply.github.com>
2021-11-17 18:02:48 +0530

    db connections

---
+++ config.js
@@ -0,0 +1,3 @@
+exports.MONGODB =
+    'mongodb+srv://twitter_admin:We1c0me@123@twitter-clone.hl9mz.mongodb.net/twitter-clone?retryWrites=true&w=majority&authSource=admin';
+exports.SECRET_KEY = 'We1c0me@123';
