7f6882fcfa20614d7633c1d27c6ab91bfa400bc4
Pranav Shinde <46496160+pra9shinde@users.noreply.github.com>
2021-11-24 19:19:48 +0530

    CORS Fix

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ const server = new ApolloServer({
 
 const app = express();
 
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'https://shindetter.netlify.app/'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 app.use('/uploads', express.static('uploads')); //Server Static files over Http
 
 server.applyMiddleware({ app });
