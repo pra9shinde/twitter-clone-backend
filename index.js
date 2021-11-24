93b584217b052305d84c494439cf729b08aa1b29
Pranav Shinde <46496160+pra9shinde@users.noreply.github.com>
2021-11-24 21:42:07 +0530

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const server = new ApolloServer({
 const app = express();
 
 app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'https://shindetter.netlify.app/'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', 'https://shindetter.netlify.app'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
